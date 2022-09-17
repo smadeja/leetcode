9e65e370ca8539b5a277b520f15cc45e56cae829
Szymon Madeja <smadeja1@gmail.com>
2022-09-17 12:30:36 +0200

    Improve the Ruby solution for "Valid Anagram"

--- valid_anagram.rb
+++ valid_anagram.rb
@@ -3,11 +3,11 @@
 def is_anagram(s, t)
   letter_count = Hash.new(0)
 
-  s.each_codepoint do |letter|
+  s.each_byte do |letter|
     letter_count[letter] += 1
   end
 
-  t.each_codepoint do |letter|
+  t.each_byte do |letter|
     new_letter_count = letter_count[letter] - 1
 
     return false if new_letter_count < 0
